17e6611decb3769ba680e7add9f9d360a8a8e6fb
Avirup Dutta (RivenFanG) <avirupdutta043@gmail.com>
2025-12-05 17:25:12 +0600

    Add UVA/11799 Horror_Dash.cpp solution

---
+++ UVA/11799 Horror_Dash.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ull unsigned long long
+#define ll long long
+#define F0R(i, n) for(int i = 0; i < n; i++)
+#define R0F(i, n) for(int i = n - 1; i >= 0; i++)
+#define FOR(i, a, b) for(int i = a; i <= b; i++)
+#define ROF(i, a, b) for(int i = b; i >= a; i--)
+#define vii vector<int>
+
+const int mod = 1e9 + 7;
+const int m = 1e6 + 7;
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int t, tc = 1; cin >> t;
+    while(t--){
+        int n, mx = INT_MIN; cin >> n;
+        F0R(i, n){
+            int a; cin >> a;
+            mx = max(a, mx);
+        }
+        cout << "Case " << tc << ": " << mx << '\n';
+        tc++;
+    }
+    return 0;
+}  
