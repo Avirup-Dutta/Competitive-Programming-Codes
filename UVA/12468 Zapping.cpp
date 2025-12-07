126bb089bab1ae7ee03bf8bf3ea9162e63722c22
Avirup Dutta (RivenFanG) <avirupdutta043@gmail.com>
2025-12-07 08:09:42 +0600

    Add UVA 12468 Zapping problem solution

---
+++ UVA/12468 Zapping.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ull unsigned long long
+#define ll long long
+#define F0R(i, n) for(int i = 0; i < n; i++)
+#define R0F(i, n) for(int i = n - 1; i >= 0; i++)
+#define FOR(i, a, b) for(int i = a; i <= b; i++)
+#define ROF(i, a, b) for(int i = b; i >= a; i--)
+#define vii vector<int>
+
+const int mod = 1e9 + 7;
+const int m = 1e6 + 7;
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int a, b;
+    while(cin >> a >> b && a >= 0 && b >= 0){
+        if(a > b) swap(a, b);
+        int f = b - a, r = a + (99 - b) + 1;
+        if(f <= r) cout << f << '\n';
+        else cout << r << '\n';
+    }
+    return 0;
+}  
