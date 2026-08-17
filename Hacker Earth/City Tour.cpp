c99c8a073016f7090476ac1f9445fd855ab11f7d
Avirup Dutta (WrongAnswerMan) <avirupdutta043@gmail.com>
2026-08-17 23:49:33 +0600

    Create City Tour.cpp

---
+++ Hacker Earth/City Tour.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+const int n = 1e5;
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int t; cin >> t;
+    while(t--){
+        long long n; cin >> n;
+		cout << n * 4 << '\n';
+    }
+    return 0;
+}
